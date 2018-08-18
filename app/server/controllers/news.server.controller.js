caff4c7e8ed7fa13026554e2e2564bc6820ca890
gcamon <ede.obinna27@gmail.com>
2018-08-18 16:42:07 +0100

    share fix

--- app/server/controllers/news.server.controller.js
+++ app/server/controllers/news.server.controller.js
@@ -51,7 +51,7 @@ exports.read = function(req,res){
 exports.renderSingle = function(req,res){
 	var model = req.model;
 	model.news.findOne({id: req.params.id},function(err,data){
-		res.render("share-single-post",{news: data});
+		res.render("single-post",{news: data});
 	})
 	
 }
