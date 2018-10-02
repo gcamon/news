29d2d27f8be6fc814a13963930ec5716ecbd5ea5
gcamon <ede.obinna27@gmail.com>
2018-10-02 08:25:09 +0100

    Search results fix

--- app/server/controllers/news.server.controller.js
+++ app/server/controllers/news.server.controller.js
@@ -262,7 +262,7 @@ function getSearchData(req,res) {
 			
 			model.news.find({category:{$not: reg},deleted:false,verified:true})
 			.sort('-pubDate')
-			.limit(15)
+			.limit(5)
 			.exec(function(err,other){
 				if(err) throw err;
 				res.render('categories',{news: data, other: other,type: 'Search Result(s) for: "' + query + '"',moment: moment});
